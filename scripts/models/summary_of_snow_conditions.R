aa3831a18b5d7502b642f3d2761add10d12c5f49
Amanda Droghini <adroghini@alaska.edu>
2018-07-01 12:41:39 -0800

    Added code to calculate daily distance travelled
    For inclusion in Results - Travel speed

--- scripts/models/summary_of_snow_conditions.R
+++ scripts/models/summary_of_snow_conditions.R
@@ -71,7 +71,28 @@ snowfall_telem %>%
   summarize(mean_depth = mean(SnowDepth), sd_depth = sd(SnowDepth), max_depth = max(SnowDepth),
     mean_accum = mean(SnowAccum), sd_accum = sd(SnowAccum), max_accum = max(SnowAccum))
 
+# What is the daily distance travelled by wolves on days of a snowfall compared to other days?
+# Does decrease in travel speed "scale up" to a decrease in daily distance travelled?
+
+# Generate dataframe
+daily.dist <- snowfall_telem %>% 
+  filter(!is.na(SnowDepth) & !is.na(speed)) %>% 
+  group_by(Device,CamDate) %>% 
+  summarize(distance = sum(steplength_m/1000))
+
+events_by_id <- snowfall_telem %>% 
+  distinct(Device, CamDate, .keep_all = TRUE) %>% 
+  select(c(Device, CamDate, snowfall_category))
+
+daily.dist <- left_join(daily.dist,events_by_id, by=c("Device" = "Device", "CamDate" = "CamDate"))
+
+# Summarize across all individuals
+daily.dist.mean <- daily.dist %>% 
+  group_by(snowfall_category) %>% 
+  summarize(mean.dist = mean(distance), st.dev = sd(distance), no.obs = length(distance))
+
+
 # Clean workspace
-rm(cams.summary, snow.tab, day_of_snow)
+rm(cams.summary, snow.tab, day_of_snow, daily.dist, events_by_id, daily.dist.mean)
 
 
